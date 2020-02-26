3a1f2f847151d9407409ea5c958849b67f4d2803
Jesper Persson <jepe6324@student.uu.se>
2020-02-26 14:41:59 +0100

    Bar + script, placeholder background, canvas

---
+++ Assets/Scripts/BarHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarHandler : MonoBehaviour
+{
+	float fillAmount = 0;
+
+	public float maxValue;
+	float currentValue = 0;
+
+	Image bar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		bar = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		fillAmount = currentValue / maxValue;
+		bar.fillAmount = fillAmount;
+	}
+
+	void setCurrentValue(float value)
+	{
+		currentValue = value;
+	}
+
+	void setMaxValue(float value)
+	{
+		maxValue = value;
+	}
+}
