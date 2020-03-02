7b4b3e7a524215d834ce2586b5e8f4bf023c20e8
Jesper Persson <jepe6324@student.uu.se>
2020-03-02 18:10:41 +0100

    Quests Scriptable Object
    Right click -> Create -> Quests

---
+++ Assets/Scripts/Quest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Quest : ScriptableObject
+{
+	public enum State
+	{
+		PENDING,
+		IN_PROGRESS,
+		DONE
+	}
+
+	public enum Area
+	{
+		VOLCANOE,
+		FOREST,
+		CITY,
+		WATERFALL
+	}
+
+	public string questDescription;
+	public int rewardPoints; // Not sure if int is the right thing to go for here, but it will do for now.
+	public string rewardText;
+	public Area region;
+
+	[HideInInspector] public State currentState = State.PENDING;
+
+	void Reset()
+	{
+		currentState = State.PENDING;
+	}
+}
---
+++ Assets/Scripts/QuestHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestHandler : MonoBehaviour
+{
+
+	/*
+	 * Planning to make it so that this handles moving the quests around for us
+	 * 
+	 * A quest with state PENDING is in it's Region's quest list.
+	 * A quest with state IN_PROGRESS is in the to do list.
+	 * A quest with state DONE is hidden until something triggers Reset().
+	 * And when a player completes a quest this box spawns the lore text box.
+	 * 
+	 * I guess this should also give the player the reward for completed quests.
+	 */
+
+	public Quest[] quests;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void Reset()
+	{
+		// Reset all quests.
+	}
+}
